3f983ce8eef5ed79320937267a872b6864fb37e7
ethanxianli <ethanxianli@tencent.com>
2022-12-29 21:35:39 +0800

    docs: pitch loader 参考

--- webpack/first-loader/loaders/pitch-loader.js
+++ webpack/first-loader/loaders/pitch-loader.js
@@ -6,6 +6,16 @@ module.exports = function(content) {
 
 module.exports.pitch = function() {
   console.debug('called pitch loader\'s pitch method', ...arguments);
+  // this.loaders 可以访问到所有配置的loader
+  // this.loaderIndex 可以获取当前loader在loaders中的索引
+  // 详细用法参考 https://github.com/webpack-contrib/thread-loader/blob/master/src/index.js
+
   // pitch 内 return 会熔断后续 loader 执行
   // return 'xxx';
+
+  // 异步也可以
+  // const callback = this.async();
+  // setTimeout(() => {
+  //   callback(null, 'xxx');
+  // })
 }
